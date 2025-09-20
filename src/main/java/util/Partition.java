a372421f8258773abb0d85b4f5194b4136f0e31f
1mmka <erhan.sapabek@gmail.com>
2025-09-20 17:13:00 +0600

    refactor(util): partition, swap, shuffle, guards

--- src/main/java/util/Partition.java
+++ src/main/java/util/Partition.java
@@ -1,4 +1,18 @@
 package util;
 
 public class Partition {
+    public static int partition(int[] arr, int left, int right, int pivotIndex) {
+        int pivotValue = arr[pivotIndex];
+        Swap.swap(arr, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (arr[i] < pivotValue) {
+                Swap.swap(arr, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        Swap.swap(arr, storeIndex, right);
+        return storeIndex;
+    }
 }
--- src/main/java/util/Shuffle.java
+++ src/main/java/util/Shuffle.java
@@ -1,4 +1,14 @@
 package util;
 
+import java.util.Random;
+
 public class Shuffle {
+    private static final Random rand = new Random();
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            Swap.swap(arr, i, j);
+        }
+    }
 }
